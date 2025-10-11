eb04dc0a011918e8f6d0f63ff7a68c3ec60ddebf
Andrei Makarov <aomak@yandex.ru>
2025-10-11 14:51:55 +0300

    делаю исправления

--- src/parser.py
+++ src/parser.py
@@ -1,13 +1,15 @@
 from abc import ABC, abstractmethod
 
+from src.file_worker import FileWorker
+
 
 class Parser(ABC):
     """Абстрактный класс для получения вакансий"""
 
-    def __init__(self, file_worker):
+    def __init__(self, file_worker: FileWorker):
         self.file_worker = file_worker
 
     @abstractmethod
-    def load_vacancies(self, keyword):
+    def load_vacancies(self, keyword: str):
         """Метод для получения вакансий"""
         pass
